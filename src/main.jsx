d43c326365c53f45fe8d55ba556b80c06cc06b74
Mahesh kumar <112768276+Mahesh09kumar@users.noreply.github.com>
2026-03-10 19:26:41 +0530

    fix vercel build error

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
-import { Analytics } from "@vercel/analytics/next";
 createRoot(document.getElementById("root")).render(
 	<BrowserRouter>
 		<StoreContextProvider>
 			<App />
-			<Analytics />
 		</StoreContextProvider>
 	</BrowserRouter>,
 );
